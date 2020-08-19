bab8bcae264c70051fd326afe2f08258fd1ec619
Somnath Paul <Somnath4x@gmail.com>
2020-08-19 20:38:09 +0530

    modified

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-names = {"python": {"in_use": "true", "version": "3.7"},
-        "flask": {"in_sue": "true", "version": "1.1.2"},
-        "django": {"in_sue": "false", "version": "none"}
+video_put_arg = reqparse.RequestParser()
+video_put_arg.add_argument("name", type=str , help={"Wrong data! Missing name."})
+video_put_arg.add_argument("views", type=int , help={"Wrong data! Missing views."})
+video_put_arg.add_argument("likes", type=int , help={"Wrong data! Missing likes."})
+video_put_arg.add_argument("dislikes", type=int , help={"Wrong data! Missing dislikes."})
+
+
+
+videos = {
        }
 
 
-class Jk(Resource):
-    def get(self, name):
-        return names[name]
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        print(request.form['likes'])
+        return {"ok": "21"}
+
+
 
 # define route 
-api.add_resource(Jk, "/jk/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "jk/flask")
-print("status code : ", response.status_code)
-print("encoding : ", response.encoding)
+
+response = requests.put(BASE + "video/1", {"likes": 10})
 print(response.json())
